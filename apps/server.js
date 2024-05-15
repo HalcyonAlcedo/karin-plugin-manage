0f6401f51be7381ec77ca44bb43e6a5ea2fd79f1
zyc404 <zyc404@gmail.com>
2024-05-15 19:29:06 +0800

    初次提交

---
+++ apps/server.js
@@ -0,0 +1,4 @@
+import server from '../lib/server/index.js'
+
+// 启动面板api
+server()
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import path from 'path'
-import { logger, common } from '#Karin'
+import { logger, common, Cfg, plugin  } from '#Karin'
+import packageConfig from './package.json' assert { type: 'json' }
 
 /** 当前文件的绝对路径 */
 const filePath = common.absPath(import.meta.url.replace(/^file:(\/\/\/|\/\/)/, ''))
 /** 插件包的目录路径 */
 const dirname = path.dirname(filePath)
 /** 插件包的名称 */
-const basename = path.basename(dirname)
+const basename = packageConfig.name
+/** 插件包的版本 */
+const version = packageConfig.version
 /** 插件包相对路径 */
 const dirPath = './plugins/' + basename
 
 export { dirPath }
 
-logger.info(basename + ' 插件 0.0.1初始化~')
+logger.info(`${basename}插件 ${version} 初始化~`)
